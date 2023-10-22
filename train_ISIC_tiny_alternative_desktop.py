1ce4a5b5aff909e267ae72cbf03b39e31363302c
Francesco Di Gangi <38263390+FDG2801@users.noreply.github.com>
2023-10-22 16:44:59 +0200

    alternative train function

--- train_ISIC_tiny_alternative_desktop.py
+++ train_ISIC_tiny_alternative_desktop.py
@@ -141,45 +141,45 @@ best_acc_final = []
 # masks_val=load_and_resize_images_efficient(val_masks,True)
 # print("masks_val ok")
 # ORIGINAL VERSION: 
-def train(model, save_name):
-    model_savedir = args.checkpoint + save_name + '/'
-    save_name = model_savedir + 'ckpt'
-    print(model_savedir)
-    if not os.path.exists(model_savedir):
-        os.mkdir(model_savedir)
-    train_ds=OnDemandISIC2017(train_csv,train_imgs, train_masks,train_transform)
-    val_ds=OnDemandISIC2017(df_val, val_imgs, val_masks,test_transform,training=False)
-    # train_ds = Mydataset(imgs_train, masks_train, train_transform)
-    # val_ds = Mydataset(imgs_val, masks_val, test_transform)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
-    CosineLR = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=1e-8)
+# def train(model, save_name):
+#     model_savedir = args.checkpoint + save_name + '/'
+#     save_name = model_savedir + 'ckpt'
+#     print(model_savedir)
+#     if not os.path.exists(model_savedir):
+#         os.mkdir(model_savedir)
+#     train_ds=OnDemandISIC2017(train_csv,train_imgs, train_masks,train_transform)
+#     val_ds=OnDemandISIC2017(df_val, val_imgs, val_masks,test_transform,training=False)
+#     # train_ds = Mydataset(imgs_train, masks_train, train_transform)
+#     # val_ds = Mydataset(imgs_val, masks_val, test_transform)
+#     criterion = nn.CrossEntropyLoss()
+#     optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
+#     CosineLR = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs, eta_min=1e-8)
 
-    # train_dl = DataLoader(train_ds, shuffle=True, batch_size=args.batch_size, pin_memory=False, num_workers=0,
-    #                       drop_last=True, )
-    # val_dl = DataLoader(val_ds, batch_size=args.batch_size, pin_memory=False, num_workers=0, )
-    train_dl = DataLoader(train_ds, shuffle=True, batch_size=16, pin_memory=False, num_workers=0, drop_last=True)
-    val_dl = DataLoader(val_ds, batch_size=16, pin_memory=False, num_workers=0)
-    best_acc = 0
-    print("Start inside train function")
-    with tqdm(total=epochs, ncols=60) as t:
-        for epoch in range(epochs):
-            epoch_loss, epoch_iou, epoch_val_loss, epoch_val_iou = \
-                fit(epoch, epochs, model, train_dl, val_dl, device, criterion, optimizer, CosineLR)
-            f = open(model_savedir + 'log' + '.txt', "a")
-            f.write('epoch' + str(float(epoch)) +
-                    '  _train_loss' + str(epoch_loss) + '  _val_loss' + str(epoch_val_loss) +
-                    ' _epoch_acc' + str(epoch_iou) + ' _val_iou' + str(epoch_val_iou) + '\n')
-            if epoch_val_iou > best_acc:
-                f.write('\n' + 'here' + '\n')
-                best_model_wts = copy.deepcopy(model.state_dict())
-                best_acc = epoch_val_iou
-                torch.save(best_model_wts, ''.join([save_name, '.pth']))
-            f.close()
-            t.update(1)
+#     # train_dl = DataLoader(train_ds, shuffle=True, batch_size=args.batch_size, pin_memory=False, num_workers=0,
+#     #                       drop_last=True, )
+#     # val_dl = DataLoader(val_ds, batch_size=args.batch_size, pin_memory=False, num_workers=0, )
+#     train_dl = DataLoader(train_ds, shuffle=True, batch_size=16, pin_memory=False, num_workers=0, drop_last=True)
+#     val_dl = DataLoader(val_ds, batch_size=16, pin_memory=False, num_workers=0)
+#     best_acc = 0
+#     print("Start inside train function")
+#     with tqdm(total=epochs, ncols=60) as t:
+#         for epoch in range(epochs):
+#             epoch_loss, epoch_iou, epoch_val_loss, epoch_val_iou = \
+#                 fit(epoch, epochs, model, train_dl, val_dl, device, criterion, optimizer, CosineLR)
+#             f = open(model_savedir + 'log' + '.txt', "a")
+#             f.write('epoch' + str(float(epoch)) +
+#                     '  _train_loss' + str(epoch_loss) + '  _val_loss' + str(epoch_val_loss) +
+#                     ' _epoch_acc' + str(epoch_iou) + ' _val_iou' + str(epoch_val_iou) + '\n')
+#             if epoch_val_iou > best_acc:
+#                 f.write('\n' + 'here' + '\n')
+#                 best_model_wts = copy.deepcopy(model.state_dict())
+#                 best_acc = epoch_val_iou
+#                 torch.save(best_model_wts, ''.join([save_name, '.pth']))
+#             f.close()
+#             t.update(1)
             
-    write_options(model_savedir, args, best_acc)
-    print('Done!')
+#     write_options(model_savedir, args, best_acc)
+#     print('Done!')
 # def train(model, save_name):
 #     model_savedir = args.checkpoint + save_name + '/'
 #     save_name = model_savedir + 'ckpt'
@@ -253,45 +253,45 @@ def train(model, save_name):
 '''
 Other train function
 '''
-# def train(model, save_name):
-#     model_savedir = args.checkpoint + save_name + '/'
-#     save_name = model_savedir + 'ckpt'
-#     print(model_savedir)
+def train(model, save_name):
+    model_savedir = args.checkpoint + save_name + '/'
+    save_name = model_savedir + 'ckpt'
+    print(model_savedir)
     
-#     if not os.path.exists(model_savedir):
-#         os.makedirs(model_savedir)
+    if not os.path.exists(model_savedir):
+        os.makedirs(model_savedir)
     
-#     train_ds = OnDemandISIC2017(train_csv, train_imgs, train_masks, train_transform)
-#     val_ds = OnDemandISIC2017(df_val, val_imgs, val_masks, test_transform, training=False)
+    train_ds = OnDemandISIC2017(train_csv, train_imgs, train_masks, train_transform)
+    val_ds = OnDemandISIC2017(df_val, val_imgs, val_masks, test_transform, training=False)
     
-#     criterion = nn.CrossEntropyLoss()
-#     optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
-#     CosineLR = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.warm_epoch + args.end_epoch, eta_min=1e-8)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
+    CosineLR = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.warm_epoch + args.end_epoch, eta_min=1e-8)
     
-#     train_dl = DataLoader(train_ds, shuffle=True, batch_size=args.batch_size, num_workers=args.workers, pin_memory=True, drop_last=True)
-#     val_dl = DataLoader(val_ds, batch_size=args.batch_size, num_workers=args.workers, pin_memory=True)
+    train_dl = DataLoader(train_ds, shuffle=True, batch_size=args.batch_size, num_workers=args.workers, pin_memory=True, drop_last=True)
+    val_dl = DataLoader(val_ds, batch_size=args.batch_size, num_workers=args.workers, pin_memory=True)
     
-#     best_acc = 0
+    best_acc = 0
     
-#     print("Start inside train function")
+    print("Start inside train function")
     
-#     with tqdm(total=args.warm_epoch + args.end_epoch, ncols=60) as t:
-#         for epoch in range(args.warm_epoch + args.end_epoch):
-#             epoch_loss, epoch_iou, epoch_val_loss, epoch_val_iou = fit(epoch, args.warm_epoch + args.end_epoch, model, train_dl, val_dl, args.device, criterion, optimizer, CosineLR)
-#             f = open(os.path.join(model_savedir, 'log.txt'), "a")
-#             f.write(f'epoch {epoch} _train_loss {epoch_loss} _val_loss {epoch_val_loss} _epoch_acc {epoch_iou} _val_iou {epoch_val_iou}\n')
+    with tqdm(total=args.warm_epoch + args.end_epoch, ncols=60) as t:
+        for epoch in range(args.warm_epoch + args.end_epoch):
+            epoch_loss, epoch_iou, epoch_val_loss, epoch_val_iou = fit(epoch, args.warm_epoch + args.end_epoch, model, train_dl, val_dl, args.device, criterion, optimizer, CosineLR)
+            f = open(os.path.join(model_savedir, 'log.txt'), "a")
+            f.write(f'epoch {epoch} _train_loss {epoch_loss} _val_loss {epoch_val_loss} _epoch_acc {epoch_iou} _val_iou {epoch_val_iou}\n')
             
-#             if epoch_val_iou > best_acc:
-#                 f.write('\n' + 'Best model found at epoch ' + str(epoch) + '\n')
-#                 best_model_wts = copy.deepcopy(model.state_dict())
-#                 best_acc = epoch_val_iou
-#                 torch.save(best_model_wts, f'{save_name}.pth')
+            if epoch_val_iou > best_acc:
+                f.write('\n' + 'Best model found at epoch ' + str(epoch) + '\n')
+                best_model_wts = copy.deepcopy(model.state_dict())
+                best_acc = epoch_val_iou
+                torch.save(best_model_wts, f'{save_name}.pth')
             
-#             f.close()
-#             t.update(1)
+            f.close()
+            t.update(1)
     
-#     write_options(model_savedir, args, best_acc)
-#     print('Training Done!')
+    write_options(model_savedir, args, best_acc)
+    print('Training Done!')
 
 
 if __name__ == '__main__':
